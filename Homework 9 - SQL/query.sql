9c1dece3cc5eb840936fb712c7caa47154c53af7
Melissa Djohan <melissadjohan6@gmail.com>
2019-10-01 23:03:53 -0400

    SQL

---
+++ Homework 9 - SQL/query.sql
@@ -0,0 +1,123 @@
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS dept_emp;
+DROP TABLE IF EXISTS dept_manager;
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS salaries;
+DROP TABLE IF EXISTS titles;
+
+CREATE TABLE departments (
+	dept_no VARCHAR NOT NULL PRIMARY KEY,
+	dept_name VARCHAR
+);
+
+SELECT * FROM departments;
+
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
+	from_date DATE DEFAULT ('now'::text)::date,
+	to_date DATE DEFAULT ('now'::text)::date
+);
+
+SELECT * FROM dept_emp;
+
+CREATE TABLE dept_manager (
+	dept_no VARCHAR NOT NULL,
+	emp_no INT NOT NULL,
+	from_date DATE DEFAULT ('now'::text)::date,
+	to_date DATE DEFAULT ('now'::text)::date
+);
+
+SELECT * FROM dept_manager;
+
+CREATE TABLE employees (
+	emp_no INT NOT NULL,
+	birth_date DATE DEFAULT ('now'::text)::date,
+	first_name VARCHAR,
+	last_name VARCHAR,
+	gender VARCHAR (1),
+	hire_date DATE DEFAULT ('now'::text)::date
+);
+
+SELECT * FROM employees;
+
+CREATE TABLE salaries (
+	emp_no INT NOT NULL PRIMARY KEY,
+	salary INT NOT NULL,
+	from_date DATE DEFAULT ('now'::text)::date,
+	to_date DATE DEFAULT ('now'::text)::date
+);
+
+SELECT * FROM salaries;
+
+CREATE TABLE titles (
+	emp_no INT NOT NULL,
+	title VARCHAR,
+	from_date DATE DEFAULT ('now'::text)::date,
+	to_date DATE DEFAULT ('now'::text)::date
+);
+
+SELECT * FROM titles;
+
+--List the following details of each employee: employee number, last name, first name, gender, and salary.
+SELECT e.emp_no, e.birth_date, e.first_name, e.last_name, e.gender, s.salary
+FROM employees AS e
+JOIN salaries AS s
+ON (e.emp_no = s.emp_no);
+
+--List employees who were hired in 1986.
+SELECT * FROM employees WHERE hire_date BETWEEN '1986-01-01' AND '1987-01-01';
+
+--List the manager of each department with the following information: department number, department name, 
+--the manager's employee number, last name, first name, and start and end employment dates.
+SELECT d.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name, dm.from_date, dm.to_date
+FROM departments AS d
+JOIN dept_manager AS dm
+ON d.dept_no = dm.dept_no
+JOIN employees AS e
+ON dm.emp_no = e.emp_no;
+
+--List the department of each employee with the following information: employee number, last name, 
+--first name, and department name.
+SELECT de.emp_no, e.last_name, e.first_name, d.dept_name
+FROM dept_emp AS de
+JOIN employees AS e
+ON de.emp_no = e.emp_no
+JOIN departments AS d
+ON de.dept_no = d.dept_no;
+
+--List all employees whose first name is "Hercules" and last names begin with "B."
+SELECT first_name, last_name
+FROM employees
+WHERE first_name = 'Hercules'
+AND last_name LIKE 'B%';
+
+--List all employees in the Sales department, including their employee number, 
+--last name, first name, and department name.
+SELECT de.emp_no, e.last_name, e.first_name, d.dept_name
+FROM dept_emp AS de
+JOIN employees AS e
+ON de.emp_no = e.emp_no
+JOIN departments AS d
+ON de.dept_no = d.dept_no
+WHERE d.dept_name = 'Sales';
+
+--List all employees in the Sales and Development departments, 
+--including their employee number, last name, first name, and department name.
+SELECT de.emp_no, e.last_name, e.first_name, d.dept_name
+FROM dept_emp AS de
+JOIN employees AS e
+ON de.emp_no = e.emp_no
+JOIN departments AS d
+ON de.dept_no = d.dept_no
+WHERE d.dept_name = 'Sales' 
+OR d.dept_name = 'Development';
+
+--In descending order, list the frequency count of employee last names, 
+--i.e., how many employees share each last name.
+SELECT last_name,
+COUNT(last_name) AS "frequency"
+FROM employees
+GROUP BY last_name
+ORDER BY
+COUNT(last_name) DESC;
