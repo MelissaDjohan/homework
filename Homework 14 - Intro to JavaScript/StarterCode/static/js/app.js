5a999a9171c22c5320f64ed7243c534a795781b0
Melissa Djohan <melissadjohan6@gmail.com>
2019-11-12 18:51:21 -0500

    push data viz hw

--- Homework 14 - Intro to JavaScript/StarterCode/static/js/app.js
+++ Homework 14 - Intro to JavaScript/StarterCode/static/js/app.js
@@ -70,90 +70,101 @@ var button = d3.select("#filter-btn");
 
 // filter for date (you will have to data.pass because the data is a string)
 button.on("click", function() {
+    d3.event.preventDefault();
     var dateInput = d3.select("#datetime");
     var dateInputValue = dateInput.property("value");
     console.log(dateInputValue);
     console.log(tableData);
     var filteredData = tableData.filter(tableData => tableData.datetime === dateInput);
-    console.log(filteredData);
+    buildTable(filteredData);
+});
 
+function buildTable(filteredDateTable) {
     filteredData.forEach((dateData) => {
         var row = tbody.append("tr");
         Object.entries(dateData).forEach(([key, value]) => {
           var cell = tbody.append("td");
           cell.text(value);
         });
-      });
 });
 
-// // filter for city
-// button.on("click", function() {
-//     var cityInput = d3.select("#city");
-//     var cityInputValue =cityInput.property("value");
-//     console.log(cityInputValue);
-//     console.log(tableData);
-//     var filteredData1 = tableData.filter(tableData => tableData.city === cityInput);
-//     console.log(filteredData1);
 
-//     filteredData1.forEach((cityData) => {
-//         var row = tbody.append("tr");
-//         Object.entries(cityData).forEach(([key, value]) => {
-//           var cell = tbody.append("td");
-//           cell.text(value);
-//         });
-//       });
-// });
+// filter for city
+button.on("click", function() {
+    var cityInput = d3.select("#city");
+    var cityInputValue =cityInput.property("value");
+    console.log(cityInputValue);
+    console.log(tableData);
+    var filteredData1 = tableData.filter(tableData => tableData.city === cityInput);
+    buildTable(filteredData1);
+});
 
-// // filter for state
-// button.on("click", function() {
-//     var stateInput = d3.select("#state");
-//     var stateInputValue =stateInput.property("value");
-//     console.log(stateInputValue);
-//     console.log(tableData);
-//     var filteredData2 = tableData.filter(tableData => tableData.state === stateInput);
-//     console.log(filteredData2);
+function buildTable(filteredCityTable) {
+    filteredData1.forEach((cityData) => {
+        var row = tbody.append("tr");
+        Object.entries(cityData).forEach(([key, value]) => {
+          var cell = tbody.append("td");
+          cell.text(value);
+        });
+      });
+};
 
-//     filteredData2.forEach((stateData) => {
-//         var row = tbody.append("tr");
-//         Object.entries(stateData).forEach(([key, value]) => {
-//           var cell = tbody.append("td");
-//           cell.text(value);
-//         });
-//       });
-// });
+// filter for state
+button.on("click", function() {
+    var stateInput = d3.select("#state");
+    var stateInputValue =stateInput.property("value");
+    console.log(stateInputValue);
+    console.log(tableData);
+    var filteredData2 = tableData.filter(tableData => tableData.state === stateInput);
+    buildTable(filteredData2);
+});
 
-// // filter for country
-// button.on("click", function() {
-//     var countryInput = d3.select("#country");
-//     var countInputValue = countryInput.property("value");
-//     console.log(countInputValue);
-//     console.log(tableData);
-//     var filteredData3 = tableData.filter(tableData => tableData.country === countryInput);
-//     console.log(filteredData3);
+function buildTable(filteredStateTable) {
+    filteredData2.forEach((stateData) => {
+        var row = tbody.append("tr");
+        Object.entries(stateData).forEach(([key, value]) => {
+          var cell = tbody.append("td");
+          cell.text(value);
+        });
+      });
+};
 
-//     filteredData3.forEach((countryData) => {
-//         var row = tbody.append("tr");
-//         Object.entries(countryData).forEach(([key, value]) => {
-//           var cell = tbody.append("td");
-//           cell.text(value);
-//         });
-//       });
-// });
+// filter for country
+button.on("click", function() {
+    var countryInput = d3.select("#country");
+    var countInputValue = countryInput.property("value");
+    console.log(countInputValue);
+    console.log(tableData);
+    var filteredData3 = tableData.filter(tableData => tableData.country === countryInput);
+    buildTable(filteredData3);
+});
 
-// // filter for shape
-// button.on("click", function() {
-//     var shapeInput = d3.select("#shape");
-//     var shapeInputValue = shapeInput.property("value");
-//     console.log(shapeInputValue);
-//     console.log(tableData);
-//     var filteredData4 = tableData.filter(tableData => tableData.shape === shapeInput);
-//     console.log(filteredData4);
+function buildTable(filteredCountryTable) {
+    filteredData3.forEach((countryData) => {
+        var row = tbody.append("tr");
+        Object.entries(countryData).forEach(([key, value]) => {
+          var cell = tbody.append("td");
+          cell.text(value);
+        });
+      });
+};
 
-//     filteredData4.forEach((shapeData) => {
-//         var row = tbody.append("tr");
-//         Object.entries(shapeData).forEach(([key, value]) => {
-//           var cell = tbody.append("td");
-//           cell.text(value);
-//         });
-//       });
-// });
+// filter for shape
+button.on("click", function() {
+    var shapeInput = d3.select("#shape");
+    var shapeInputValue = shapeInput.property("value");
+    console.log(shapeInputValue);
+    console.log(tableData);
+    var filteredData4 = tableData.filter(tableData => tableData.shape === shapeInput);
+    buildTable(filteredData4);
+  });
+
+function buildTable(filteredShapeTable) {
+    filteredData4.forEach((shapeData) => {
+        var row = tbody.append("tr");
+        Object.entries(shapeData).forEach(([key, value]) => {
+          var cell = tbody.append("td");
+          cell.text(value);
+        });
+      });
+};
